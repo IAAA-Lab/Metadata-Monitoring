2814b87b85bd75efd6f7e2e0e9bc4e4db62d118f
sergio <sergiomartinez_1997@hotmail.com>
2022-07-14 17:44:11 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'metadata-monitoring';
+
+
+  getForm(mqa: HTMLInputElement, iso19157: HTMLInputElement, sparql: HTMLInputElement, ckan: HTMLInputElement,
+          nti: HTMLInputElement, dcat_ap: HTMLInputElement, direct: HTMLInputElement, local: HTMLInputElement): void {
+    console.log("mqa: " + mqa.checked)
+    console.log("iso19157: " + iso19157.checked)
+    console.log("sparql: " + sparql.checked)
+    console.log("ckan: " + ckan.checked)
+    console.log("nti: " + nti.checked)
+    console.log("dcat_ap: " + dcat_ap.checked)
+    console.log("direct: " + direct.checked)
+    console.log("local: " + local.checked)
+  }
 }
