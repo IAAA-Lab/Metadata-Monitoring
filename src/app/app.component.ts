c78cc9db35ad82492ea210306d7c795f31e2702e
sergio <sergiomartinez_1997@hotmail.com>
2022-07-15 09:56:25 +0200

    Reestructuración componentes y añadido routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,20 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'metadata-monitoring';
-
-
-  getForm(mqa: HTMLInputElement, iso19157: HTMLInputElement, sparql: HTMLInputElement, ckan: HTMLInputElement,
-          nti: HTMLInputElement, dcat_ap: HTMLInputElement, direct: HTMLInputElement, local: HTMLInputElement, texto: HTMLInputElement): void {
-    console.log("mqa: " + mqa.checked)
-    console.log("iso19157: " + iso19157.checked)
-    console.log("sparql: " + sparql.checked)
-    console.log("ckan: " + ckan.checked)
-    console.log("nti: " + nti.checked)
-    console.log("dcat_ap: " + dcat_ap.checked)
-    console.log("direct: " + direct.checked)
-    console.log("local: " + local.checked)
-    console.log("TEXTO: " + texto.value)
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResultsComponent } from './results/results.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { EvaluationComponent } from './evaluation/evaluation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultsComponent,
+    EvaluationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
