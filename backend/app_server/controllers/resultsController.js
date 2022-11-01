19050500ec6005141f672750da3e39fd516e22dc
sergiomartinez_1997@hotmail.com <sergiomartinez_1997@hotmail.com>
2022-11-01 18:57:45 +0100

    Devuelve un unico json con todos los índices

--- backend/app_server/controllers/resultsController.js
+++ backend/app_server/controllers/resultsController.js
@@ -3,33 +3,30 @@ const { Results_mqa_sparql, Results_ISO19157 } = require('./schema')
 
 //Return every 'URL - DATE - MQA/ISO19157' in the database
 const resultsIndex = async function (req, res) {
-    let result_mqa = await Results_mqa_sparql.find({}).then(r => {
-        let result = []
+    let indexes = []
+    await Results_mqa_sparql.find({}).then(r => {
         for(let i=0; i<r.length; i++) {
             let property = {
                 URL: r[i].URL,
                 Date: r[i].Date,
                 Method: 'MQA'
             }
-            result.push(property)
+            indexes.push(property)
         }
-        return result
     })
 
-    let result_ISO19157 = await Results_ISO19157.find({}).then(r => {
-        let result = []
+    await Results_ISO19157.find({}).then(r => {
         for(let i=0; i<r.length; i++) {
             let property = {
                 URL: r[i].URL,
                 Date: r[i].Date,
                 Method: 'ISO19157'
             }
-            result.push(property)
+            indexes.push(property)
         }
-        return result
     })
-    //Concatenar los dos arrays
-    res.json(result_mqa)
+
+    res.json(indexes)
 }
 
 module.exports = {
